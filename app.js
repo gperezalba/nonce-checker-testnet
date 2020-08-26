22cdb1a59f878a6e98180b82d281c34fa0a03e91
gperezalba <gperezalba94@gmail.com>
2020-08-26 11:10:38 +0200

    front update

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const URL = "http://52.28.106.166:23001";
 
+const MANUEL1 = "";
+const MANUEL2 = "";
+const MANUEL3 = "";
+
+const JOHAN1 = "";
+const JOHAN2 = "";
+const JOHAN3 = "";
+
 const GUILLE1 = "0xf93e2E6B8018D4c3aaf455cfE2b20463E10162d0";
 const GUILLE2 = "0xA246C82ddd6D13c8C2cBF7A6e2838A9e65c8DA19";
 const GUILLE3 = "0xa9C4b0EB0Ede565539B6E80cd14096B3f276F79A";
